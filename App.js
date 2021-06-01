3d2d64b515892c2de78251b9b633fb7cab4be3c7
auhameed <uabdulhameed1@gmail.com>
2021-06-01 12:36:22 -0700

    Added changes

---
+++ App.js
@@ -0,0 +1,29 @@
+import {
+  Route,
+  NavLink,
+  HashRouter,
+  Redirect,
+  useHistory 
+} from "react-router-dom"; 
+import Home from './Component/Home';
+import Profile from './Component/Profile';
+import Login from './Component/Login';
+import Register from './Component/Register';
+
+
+function App() {
+  return (
+    <HashRouter>
+      <div className="content">
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={() => <Login type={1} addProfile={addProfile} />} />
+        <Route exact path="/register" component={() => <Login type={2} addProfile={addProfile} />} />
+        { profile !== '' &&
+          <Route exact path="/profile" component={Register} />
+        }
+      </div>
+    </HashRouter>
+  );
+}
+
+export default App;
---
+++ Component/Home.js
@@ -0,0 +1,17 @@
+
+const Home = () => {
+    return(
+        <div id="heroimage">
+            <div className="text-center">
+                <div className="hero-text">
+                    <h1 id="h1">Welcome</h1>
+                    <hr />
+                        <p>Hello World!</p>
+			    </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default Home
